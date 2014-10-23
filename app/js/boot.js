c9cdbbac67c35d6f9ed65a7bc9673622c449dd34
Chalos Yance Lawrence <cyl_05019@hotmail.com>
2014-10-23 19:23:15 +0800

    Revert ".."

--- app/js/boot.js
+++ app/js/boot.js
@@ -1,24 +1,19 @@
 ﻿/*
- * 這裏是整支程式的進入點，它負責建立 root view，
- * 也就是 MainApp 元件，將它建立起來放到畫面上
+ * 這支是程式進入點，它負責建立 root view (controller view)，
+ * 也就是 TodoApp 這個元件
  *
  * boot.js 存在的目地，是因為通常 app 啟動時有許多先期工作要完成，
  * 例如預載資料到 store 內、檢查本地端 db 狀態、切換不同語系字串、
- * 這些工作都先在 boot.js 內做完，再啟動 root view 是比較理想的流程
+ * 這些工作都先在 boot.js 內做完，再啟動 TodoApp view 建立畫面是比較好的
  * 
  */
 
-// v0.12 開始要用 createFactory 包一次才能使用元件
-// 如果不希望這麼麻煩，只要在每份 js 裏都加下面這句即可，但它有缺點
-// var React = require('react');
-// 
-// 因為 require('...') 只是拿到一份元件定義檔，無法直接使用
-// 要用它建立一個 factory，之後才能產出 instance，下面 createFactory() 就是在建立工廠
+// v0.12 開始要用 factory 包一次才能直接呼叫	
 var MainApp = React.createFactory(require('./views/MainApp.jsx'));
 
 $(function(){
 
-	// 
+	// 啟動 root view 時要傳入假資料
 	React.render( MainApp(), document.getElementById('container') );
 
 })
--- app/js/stores/TodoStore.js
+++ app/js/stores/TodoStore.js
@@ -10,57 +10,48 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 var actions = require('../actions/AppActionCreator');
 
+var objectAssign = require('object-assign');
 var EventEmitter = require('events').EventEmitter; // 取得一個 pub/sub 廣播器
-
-//========================================================================
-//
-// Private vars
-
-// 等同於 TodoStore extends EventEmitter 
-// 從此取得廣播的能力
-// 由於將來會返還 TodoStore 出去，因此下面寫的會全變為 public methods
-var Store = new EventEmitter();
-
-// 假資料
-var arrTodos = null;
-
-// 目前選取的 todo 項目
-var selectedItem = null;
-
-// header 裏隨打即查輸入的文字
-var searchFilter = '';
-
-// app 第一次啟動時，存入一包 mock data 到 localStorage 供測試
-var db = window.localStorage;
-if( db.hasOwnProperty('mydb') == false ){
-    // console.log( '\n無歷史資料，存入 mock data' );
-    db.setItem('mydb', JSON.stringify({todos: [], selectedItem: null}) )
-}
-
-// 接著一律從 db 讀取歷史資料
-var o = JSON.parse(db.getItem('mydb'));
-arrTodos = o.todos ? o.todos : [] ;
-selectedItem = o.selectedItem;
 
 //========================================================================
 //
 // Public API
 
+// 等同於 TodoStore extends EventEmitter 
+// 從此取得廣播的能力
+// 由於將來會返還 TodoStore 出去，因此下面寫的會全變為 public methods
+var Store = {};
+
+// 該此的 global variable，不要學...
+window.cnt = 1;
+
+// 假資料
+var arrTodos = [
+    {name: '待辦事項 1', created: Date.now(), uid: cnt++},
+    {name: '待辦事項 2', created: Date.now(), uid: cnt++}
+];
+
+// 目前選取的 todo 項目
+var selectedItem = null;
+
 /**
  * 建立 Store class，並且繼承 EventEMitter 以擁有廣播功能
  */
-$.extend( Store, {
+objectAssign( Store, EventEmitter.prototype, {
 
     /**
      * Public API
      * 供外界取得 store 內部資料
      */
-    getAll: function(){
-        return {
-            arrTodos: arrTodos,
-            selectedItem: selectedItem,
-            filter: searchFilter
-        }
+    getTodos: function(){
+        return arrTodos;
+    },
+
+    /**
+     * 
+     */
+    getSelectedItem: function(){
+        return selectedItem;
     },
 
     //
@@ -71,10 +62,6 @@ $.extend( Store, {
 //
 // event handlers
 
-/**
- * 向 Dispatcher 註冊自已，才能偵聽到系統發出的事件
- * 並且取回 dispatchToken 供日後 async 操作用
- */
 Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
 
     // evt .action 就是 view 當時廣播出來的整包物件
@@ -90,14 +77,9 @@ Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
 
             arrTodos.push( action.item );
 
-            // console.log( 'Store 新增: ', arrTodos );
-
-            // 將新增的項目設為 selected，將來在 ui 裏會高亮與自動捲動
-            selectedItem = action.item;
+            console.log( 'Store 新增: ', arrTodos );
 
             Store.emit( AppConstants.CHANGE_EVENT );
-
-            persist();
                 
             break;
 
@@ -110,16 +92,9 @@ Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
                 return item != action.item;
             })
 
-            // 如果當前選取的 item 被刪掉了，要記錄這個事實
-            if( selectedItem == action.item ){
-                selectedItem = null;
-            }
-
-            // console.log( 'Store 刪完: ', arrTodos );
+            console.log( 'Store 刪完: ', arrTodos );
 
             Store.emit( AppConstants.CHANGE_EVENT );
-
-            persist();
                 
             break;
 
@@ -128,13 +103,9 @@ Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
          */    
         case AppConstants.TODO_UPDATE:
 
-            console.log( 'Store 更新: ', action.item );
-            
-            action.item.name = action.newVal;
+            console.log( 'Store 更新: ', arrTodos );
 
             Store.emit( AppConstants.CHANGE_EVENT );
-
-            persist();
                 
             break;
 
@@ -143,28 +114,14 @@ Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
          */    
         case AppConstants.TODO_SELECT:
 
-            // console.log( 'Store 選取: ', action.item );
+            console.log( 'Store 選取: ', action.item );
 
             // 選取同樣的 item 就不用處理下去了
             if( selectedItem != action.item ){
                 selectedItem = action.item;
                 Store.emit( AppConstants.CHANGE_EVENT );
-                persist();
             }
-                
-            break;
 
-        /**
-         * 
-         */    
-        case AppConstants.TODO_FILTER:
-
-            // console.log( 'Store 查詢: ', action.val );
-
-            if( searchFilter != action.val ){
-                searchFilter = action.val
-                Store.emit( AppConstants.CHANGE_EVENT );
-            }
                 
             break;
 
@@ -175,17 +132,6 @@ Store.dispatchToken = AppDispatcher.register( function eventHandlers(evt){
     }
 
 })
-
-//========================================================================
-//
-// private methods
-
-/**
- * 將資料保存入 localStorage，下次開啟時取回
- */
-function persist(){
-    db.setItem('mydb', JSON.stringify({todos: arrTodos, selectedItem: selectedItem}) );
-}
 
 //
 module.exports = Store;
